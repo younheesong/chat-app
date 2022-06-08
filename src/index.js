1e62583493cbb56d993fb9d4cbd56832d4fe8098
younhee song <thd6517@naver.com>
2022-06-08 22:26:11 +0900

    Feat: redux setting

--- src/index.js
+++ src/index.js
@@ -6,10 +6,29 @@ import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { creatStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./redux/reducer";
+
+const createStoreWthiMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(creatStore);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider
+      store={createStoreWthiMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION()
+      )}
+    >
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
