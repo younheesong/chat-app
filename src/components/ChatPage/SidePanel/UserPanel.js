ffe1c7c93e4c61c2f445fc837d8066e214d79fe3
younheesong <thd6517@naver.com>
2022-06-20 22:55:04 +0900

    feat: user panel

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { IoIosChatboxes } from "react-icons/io";
+import Dropdown from "react-bootstrap/Dropdown";
+import Image from "react-bootstrap/Image";
+import { useSelector } from "react-redux";
 
 function UserPanel() {
-  return <div>UserPanel</div>;
+  const user = useSelector((state) => state.user.currentUser);
+
+  return (
+    <div>
+      {/* logo */}
+      <h3>
+        <IoIosChatboxes /> chat app
+      </h3>
+
+      <div style={{ display: "flex", marginBottom: "1rem" }}>
+        <Image
+          src={user && user.photoURL}
+          roundedCircle
+          style={{ width: "30px", height: "30px", marginTop: "3px" }}
+        />
+        <Dropdown>
+          <Dropdown.Toggle
+            style={{ background: "transparent", border: "0px" }}
+            variant="success"
+            id="dropdown-basic"
+          >
+            user name
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item>로그아웃</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
 
 export default UserPanel;
