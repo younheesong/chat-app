cf479611a8c2bcd8d6b7822c0353837c3a6901f0
younheesong <thd6517@naver.com>
2022-06-24 00:02:17 +0900

    feat: message send ui & message save database

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import Form from "react-bootstrap/Form";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import firebase from "../../../firebase";
+import { useSelector } from "react-redux";
+import { getDatabase, ref, set, remove, push, child } from "firebase/database";
 
 function MessageForm() {
-  return <div>MessageForm</div>;
+  const chatRoom = useSelector((state) => state.chatRoom.room);
+  const user = useSelector((state) => state.user.currentUser);
+  const [content, setContent] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const messagesRef = ref(getDatabase(), "messages");
+  const createMessage = (fileUrl = null) => {
+    const message = {
+      timestamp: new Date(),
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+    if (fileUrl !== null) {
+      message["image"] = fileUrl;
+    } else {
+      message["content"] = content;
+    }
+    return message;
+  };
+  const handleSubmit = async () => {
+    if (!content) {
+      setErrors((prev) => prev.concat("type content require"));
+      return;
+    }
+    setLoading(true);
+    //firebase에 메세지 저장.
+    try {
+      console.log("hi");
+      await set(push(child(messagesRef, chatRoom.id)), createMessage());
+      setLoading(false);
+      setContent("");
+      setErrors([]);
+    } catch (error) {
+      console.log("zz");
+      setLoading(false);
+      setErrors((pre) => pre.concat(error.message));
+    }
+  };
+  const handleChange = (e) => {
+    setContent(e.target.value);
+  };
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Control
+            // onKeyDown={handleKeyDown}
+            value={content}
+            onChange={handleChange}
+            as="textarea"
+            rows={3}
+          />
+        </Form.Group>
+      </Form>
+
+      {/* {!(percentage === 0 || percentage === 100) && (
+        <ProgressBar
+          variant="warning"
+          label={`${percentage}%`}
+          now={percentage}
+        />
+      )} */}
+
+      <div>
+        {errors.map((errorMsg) => (
+          <p style={{ color: "red" }} key={errorMsg}>
+            {errorMsg}
+          </p>
+        ))}
+      </div>
+
+      <Row>
+        <Col>
+          <button
+            onClick={handleSubmit}
+            className="message-form-button"
+            style={{ width: "100%" }}
+            disabled={loading ? true : false}
+          >
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button
+            // onClick={handleOpenImageRef}
+            className="message-form-button"
+            style={{ width: "100%" }}
+            disabled={loading ? true : false}
+          >
+            UPLOAD
+          </button>
+        </Col>
+      </Row>
+
+      <input
+        accept="image/jpeg, image/png"
+        style={{ display: "none" }}
+        type="file"
+        // ref={inputOpenImageRef}
+        // onChange={handleUploadImage}
+      />
+    </div>
+  );
 }
 
 export default MessageForm;
